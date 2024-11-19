d9bfffb29b175821be5206b95da420f95bdb6d71
izabella-mastroianni <67485849+izabella-mastroianni@users.noreply.github.com>
2024-11-18 19:04:20 -0500

    Updated minimap2_alignment.sh code

--- scripts/minimap2_alignment.sh
+++ scripts/minimap2_alignment.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-# download and install minimap2
-curl -L https://github.com/lh3/minimap2/releases/download/v2.28/minimap2-2.28_x64-linux.tar.bz2 | tar -jxvf -
-./minimap2-2.28_x64-linux/minimap2
-
-# download genome files to align
-
+# set variable for reference genome and index of it (so can be changed later)
+REFERENCE_GENOME_MK="MK373780_1.fasta"
+REFERENCE_INDEX_MK="reference_genome_mk.mmi"
 
 # index the reference genome 
-minimap2 -d reference_genome.mmi reference_genome.fasta
+minimap2 -d $REFERENCE_INDEX_MK $REFERENCE_GENOME_MK
 
-# define indexed reference genome variable
-reference_index = ".mmi"
-
-# define genomes
-genomes = (".fasta", ".fasta" )
+# define genomes, including path to folder they are in 
+GENOMES=("genomes/OR062524_1.fasta" "genomes/OR062525_1.fasta" "genomes/OR062526_1.fasta" "genomes/OR062527_1.fasta" "genomes/OR062528_1.fasta" "genomes/OR062529_1.fasta" "genomes/OR062530_1.fasta")
 
 # define output directory for PAF files
-OUTPUT_DIR = "paf_output"
+OUTPUT_DIR="paf_output"
 mkdir -p $OUTPUT_DIR
 
 
 # use for loop to run minimap2 on each genome to align to reference
-for phage_genome in "${genomes[@]}"; do
+for PHAGE_GENOME in "${GENOMES[@]}"; do
+    # get the base name of the phage genome file 
+    BASENAME=$(basename "$PHAGE_GENOME" .fasta)
+
+    # run minimap2 for each phage genome against the reference genome
+    minimap2 -a $REFERENCE_INDEX_MK $PHAGE_GENOME > "$OUTPUT_DIR/$BASENAME.paf"
+
+    echo "Alignment of $PHAGE_GENOME complete. Output: $OUTPUT_DIR/$BASENAME.paf"
+done
+
